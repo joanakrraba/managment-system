54dc85b3c2aacc7bd1fc3710d43e9d6b0128dd5f
Senad Shabaj <senadshabaj73@gmail.com>
2024-07-06 13:08:33 +0200

    Updated navbar

--- task_management/urls.py
+++ task_management/urls.py
@@ -19,7 +19,11 @@ Including another URLconf
 
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('client_list/', views.client_list, name='client_list'),
     path('project_list/', views.project_list, name='project_list'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
